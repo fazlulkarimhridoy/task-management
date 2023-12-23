5ed27dc922251eb6bc44b4accc20fef19ff8167e
fazlulkarimhridoy <fkhridoy4321@gmail.com>
2023-12-23 16:59:32 +0600

    updated

--- src/Layout/Dash.jsx
+++ src/Layout/Dash.jsx
@@ -27,43 +27,13 @@ const Dash = () => {
       <div className="flex">
         {/* dashboard sidebar */}
         <div className="min-h-screen p-3 space-y-2 w-60 bg-gray-100 text-gray-800">
-          {/* <div className="flex items-center p-2 space-x-4">
-            <img
-              src={image}
-              alt="user_photo"
-              className="w-12 h-12 rounded-full bg-gray-500"
-            />
-            <div>
-              <h2 className="text-lg font-semibold">{name}</h2>
-              <span className="flex items-center space-x-1">
-                <Link
-                  to="/profile"
-                  className="text-xs hover:underline text-gray-600"
-                >
-                  View profile
-                </Link>
-              </span>
-            </div>
-          </div> */}
-          <div className="divide-y divide-gray-300">
+          <div className="divide-y divide-gray-300 pt-10">
             <ul
               id="link1"
-              className="menu menu-vertical px-1 gap-2 text-gray-500"
+              className="menu menu-vertical px-1 gap-4 text-gray-500"
             >
               {links}
             </ul>
-            {/* <ul className="pt-4 pb-2 space-y-1 text-sm">
-              <li>
-                <button
-                  onClick={() => logout()}
-                  rel="noopener noreferrer"
-                  className="flex items-center p-2 space-x-3 rounded-md"
-                >
-                  <FaSignOutAlt></FaSignOutAlt>
-                  <span>Logout</span>
-                </button>
-              </li>
-            </ul> */}
           </div>
         </div>
 
--- src/Pages/Dashboard/TaskDashboard/TaskDashboard.jsx
+++ src/Pages/Dashboard/TaskDashboard/TaskDashboard.jsx
@@ -1,80 +1,117 @@
+import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../../Hooks/useAxiosPublic";
+import { useEffect, useState } from "react";
 
 const TaskDashboard = () => {
     // hooks and states
+    const [todo, setTodo] = useState([]);
+    const [ongoing, setOngoing] = useState([]);
+    const [completed, setCompleted] = useState([]);
     const axiosPublic = useAxiosPublic();
 
     // react query to fetch data from mongodb
-    
+    const { data: tasks = [], isPending } = useQuery({
+        queryKey: ["tasks"],
+        queryFn: async () => {
+            const res = await axiosPublic.get("/tasks");
+            return res.data;
+        }
+    })
 
+
+    // useEffect for steps
+    useEffect(() => {
+        const todoData = tasks?.filter(data => data.status === "todo");
+        const ongoingData = tasks?.filter(data => data.status === "ongoing");
+        const completedData = tasks?.filter(data => data.status === "completed");
+        setTodo(todoData);
+        setOngoing(ongoingData);
+        setCompleted(completedData)
+    }, [tasks])
+
+    // showing loader if data is pending
+    if (isPending) {
+        return <div className="flex flex-row gap-4 w-full">
+            <div className="skeleton h-96 w-full"></div>
+            <div className="skeleton h-96 w-full"></div>
+            <div className="skeleton h-96 w-full"></div>
+        </div>
+    }
 
     return (
         <div>
-            <div className="flex items-center gap-4">
+            <div className="flex gap-4">
 
                 {/* to-do list */}
                 <div className="rounded-xl border border-gray-700 bg-gray-300 p-4 text-black w-1/3">
-                    <div>
-                        <h3 className="text-3xl font-bold text-center">To-do</h3>
+                    <div className="mb-5">
+                        <h3 className="text-3xl font-bold text-center">To-do ({todo.length})</h3>
                     </div>
                     <ul className="mt-4 space-y-2">
-                        <li className="bg-green-50 rounded-lg">
-                            <p className="block h-full rounded-lg border-gray-700 p-4 hover:border-pink-600">
-                                <strong className="font-medium ">Project A</strong>
+                        {
+                            todo?.map(item => <li key={item._id} className="bg-sky-50 rounded-lg">
+                                <div className="block h-full rounded-lg border-gray-50 p-4 border hover:border-pink-600">
+                                    <strong className="font-medium ">{item.title}</strong>
 
-                                <p className="mt-1 text-xs font-medium text-gray-600">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime consequuntur deleniti, unde ab ut in!
-                                </p>
-                                <div className="flex items-center justify-between mt-2">
-                                    <p className="text-xs font-semibold text-gray-400">Deadline : 12-12-2023</p>
-                                    <p className="text-xs font-semibold text-gray-400 pr-5">Priority : High</p>
+                                    <div className="mt-1 text-xs font-medium text-gray-600">
+                                        {item.description}
+                                    </div>
+                                    <div className="flex items-center justify-between mt-2">
+                                        <p className="text-xs font-semibold text-gray-400">Deadline : {item.deadline}</p>
+                                        <p className="text-xs font-semibold text-gray-400 pr-5">Priority : {item.priority}</p>
+                                    </div>
                                 </div>
-                            </p>
-                        </li>
+                            </li>)
+                        }
+
                     </ul>
                 </div>
 
                 {/* on-going list */}
                 <div className="rounded-xl border border-gray-700 bg-gray-200 p-4 text-black w-1/3">
                     <div>
-                        <h3 className="text-3xl font-bold text-center">On-going</h3>
+                        <h3 className="text-3xl font-bold text-center">On-going ({ongoing.length})</h3>
                     </div>
                     <ul className="mt-4 space-y-2">
-                    <li className="bg-green-50 rounded-lg">
-                            <p className="block h-full rounded-lg border-gray-700 p-4 hover:border-pink-600">
-                                <strong className="font-medium ">Project A</strong>
+                        {
+                            ongoing?.map(item => <li key={item._id} className="bg-lime-50 rounded-lg">
+                                <div className="block h-full rounded-lg border-gray-50 border p-4 hover:border-pink-600">
+                                    <strong className="font-medium ">{item.title}</strong>
 
-                                <p className="mt-1 text-xs font-medium text-gray-600">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime consequuntur deleniti, unde ab ut in!
-                                </p>
-                                <div className="flex items-center justify-between mt-2">
-                                    <p className="text-xs font-semibold text-gray-400">Deadline : 12-12-2023</p>
-                                    <p className="text-xs font-semibold text-gray-400 pr-5">Priority : High</p>
+                                    <div className="mt-1 text-xs font-medium text-gray-600">
+                                        {item.description}
+                                    </div>
+                                    <div className="flex items-center justify-between mt-2">
+                                        <p className="text-xs font-semibold text-gray-400">Deadline : {item.deadline}</p>
+                                        <p className="text-xs font-semibold text-gray-400 pr-5">Priority : {item.priority}</p>
+                                    </div>
                                 </div>
-                            </p>
-                        </li>
+                            </li>)
+                        }
                     </ul>
                 </div>
 
                 {/* completed list */}
                 <div className="rounded-xl border border-gray-700 bg-gray-100 p-4 text-black w-1/3">
                     <div>
-                        <h3 className="text-3xl font-bold text-center">Completed</h3>
+                        <h3 className="text-3xl font-bold text-center">Completed ({completed.length})</h3>
                     </div>
                     <ul className="mt-4 space-y-2">
-                    <li className="bg-green-50 rounded-lg">
-                            <p className="block h-full rounded-lg border-gray-700 p-4 hover:border-pink-600">
-                                <strong className="font-medium ">Project A</strong>
+                        {
+                            completed?.map(item => <li key={item._id} className="bg-green-50 rounded-lg">
+                                <div className="block h-full rounded-lg border-gray-50 border p-4 hover:border-pink-600">
+                                    <strong className="font-medium ">{item.title}</strong>
 
-                                <p className="mt-1 text-xs font-medium text-gray-600">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime consequuntur deleniti, unde ab ut in!
-                                </p>
-                                <div className="flex items-center justify-between mt-2">
-                                    <p className="text-xs font-semibold text-gray-400">Deadline : 12-12-2023</p>
-                                    <p className="text-xs font-semibold text-gray-400 pr-5">Priority : High</p>
+                                    <div className="mt-1 text-xs font-medium text-gray-600">
+                                        {item.description}
+                                    </div>
+                                    <div className="flex items-center justify-between mt-2">
+                                        <p className="text-xs font-semibold text-gray-400">Deadline : {item.deadline}</p>
+                                        <p className="text-xs font-semibold text-gray-400 pr-5">Priority : {item.priority}</p>
+                                    </div>
                                 </div>
-                            </p>
-                        </li>
+                            </li>)
+                        }
                     </ul>
                 </div>
 
