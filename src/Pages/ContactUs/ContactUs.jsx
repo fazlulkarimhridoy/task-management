141b1f8bfdd34e5c4216333bdd9cfa1ec0140cbd
fazlulkarimhridoy <fkhridoy4321@gmail.com>
2023-12-25 03:34:21 +0600

    updated user catergory

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -2,7 +2,7 @@ import contactImage from "../../assets/ContactUsImages/contact1.png"
 const ContactUs = () => {
     return (
         <div className="bg-gray-100 min-h-screen">
-            <div className="grid grid-cols-1 pt-48 h-[600px] gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 bg-gray-100 text-gray-800">
+            <div className="grid grid-cols-1 pt-48 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 bg-gray-100 text-gray-800">
                 <div className="flex flex-col justify-between">
                     <div className="space-y-2">
                         <h2 className="text-4xl font-bold leadi lg:text-5xl">Lets talk!</h2>
