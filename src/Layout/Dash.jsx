73247d6a672124c352e8d6afa42889b5be1afb6b
fazlulkarimhridoy <fkhridoy4321@gmail.com>
2023-12-25 15:56:01 +0600

    added user information in the dashboard sidebar

--- src/Layout/Dash.jsx
+++ src/Layout/Dash.jsx
@@ -1,7 +1,12 @@
 import { Link, NavLink, Outlet } from "react-router-dom";
 import { FaCheck, FaDatabase, FaHome, FaHourglass, FaPlus, FaUndoAlt } from "react-icons/fa";
+import useAuth from "../Hooks/useAuth";
 
 const Dash = () => {
+  // states and hooks
+  const { user } = useAuth();
+
+  // links
   const links = (
     <>
       <li>
@@ -43,15 +48,15 @@ const Dash = () => {
         {/* dashboard sidebar */}
         <div className="min-h-screen p-3 space-y-2 w-60 bg-gray-100 text-gray-800">
           <div className="flex items-center p-2 space-x-4">
-            <img src="" alt="user_photo" className="w-12 h-12 rounded-full bg-gray-500" />
+            <img src={user.photoURL} alt="user_photo" className="w-12 h-12 rounded-full bg-gray-500" />
             <div>
-              <h2 className="text-lg font-semibold">Fazlul Karim</h2>
+              <h2 className="text-sm font-semibold">{user.displayName}</h2>
               <span className="flex items-center space-x-1">
                 <Link to="/profile" className="text-xs hover:underline text-gray-600">View profile</Link>
               </span>
             </div>
           </div>
-          <div className="divide-y divide-gray-300 pt-10">
+          <div className="divide-y divide-gray-300">
             <ul
               id="link1"
               className="menu menu-vertical px-1 gap-4 text-gray-500"
