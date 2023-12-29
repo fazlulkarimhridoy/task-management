363afa5160726217a2334ec7adaee1f5042fb341
fazlulkarimhridoy <fkhridoy4321@gmail.com>
2023-12-30 05:02:13 +0600

    updated mobile responsive in dashboard

--- src/Layout/Dash.jsx
+++ src/Layout/Dash.jsx
@@ -44,7 +44,7 @@ const Dash = () => {
   );
   return (
     <div className="container mx-auto">
-      <div className="flex">
+      <div className="flex flex-col lg:flex-row">
         {/* dashboard sidebar */}
         <div className="hidden lg:flex">
           <div className="min-h-screen p-3 space-y-2 w-60 bg-gray-100 text-gray-800">
@@ -66,6 +66,31 @@ const Dash = () => {
               </ul>
             </div>
           </div>
+        </div>
+        <div className="dropdown bg-gray-200">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            id="link1"
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {links}
+          </ul>
         </div>
 
         {/* dashboard content */}
